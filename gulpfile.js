09831031f7811144460eefb52ff126fdca7f97b4
Francisco <kolia.boychak@gmail.com>
2024-01-18 17:43:02 +0200

    Created the structure

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+
+const paths = {
+  scss: 'scss/**/*.scss',
+  dest: 'css'
+};
+
+gulp.task('styles', function() {
+  return gulp.src(paths.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.scss, gulp.series('styles'));
+});
+
+gulp.task('default', gulp.series('styles', 'watch'));
